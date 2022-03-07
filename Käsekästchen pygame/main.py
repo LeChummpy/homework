9fbb85efba4da2dcd4bb1ea46131949ff8eb54c2
LeChummpy <benjamin.jank@gmx.de>
2022-03-07 22:17:40 +0100

    asdasd

--- Käsekästchen pygame/main.py
+++ Käsekästchen pygame/main.py
@@ -2,13 +2,14 @@ import pygame
 import mainclasses
 import datastructures
 import colors
+import networkutils
 from helper import *
 
 pygame.init()
 
 #-----------------------------------------------------------
 #fonts
-prettyfont = pygame.font.SysFont('Arial', 70)
+prettyfont = pygame.font.SysFont('Arial', 60)
 prettyfontsmaller = pygame.font.SysFont('Arial', 35)
 
 #-----------------------------------------------------------
@@ -23,8 +24,10 @@ class Game():
 
 class Menu():
     def view(self):
-        text_spielen_x, text_spielen_y = screen_width//2-100, screen_height//2-100
-        text_beenden_x, text_beenden_y = screen_width//2-110, screen_height//2-25
+        text_raumbeitreten_x, text_raumbeitreten_y =screen_width//2-300, screen_height//2-150
+        text_raumhosten_x, text_raumhosten_y = screen_width//2-275, screen_height//2-75
+        text_lokal_x, text_lokal_y = screen_width//2-200, screen_height//2
+        text_beenden_x, text_beenden_y = screen_width//2-100, screen_height//2+75
 
         screen.fill(colors.lime)
 
@@ -35,24 +38,51 @@ class Menu():
                 pygame.quit()
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if Kords[0]>text_spielen_x and Kords[0]<text_spielen_x+200 and Kords[1]>text_spielen_y and Kords[1]<text_spielen_y+75:
-                    G.current_view = ActualGame(5, 5)
+
+                if Kords[0]>text_raumbeitreten_x and Kords[0]<text_raumbeitreten_x+650 and Kords[1]>text_raumbeitreten_y and Kords[1]<text_raumbeitreten_y+75:
+                    G.current_view = MehrspielerRaumbeireten(5, 5)
+
+                elif Kords[0]>text_raumhosten_x and Kords[0]<text_raumhosten_x+600 and Kords[1]>text_raumhosten_y and Kords[1]<text_raumhosten_y+75:
+                    G.current_view = MehrspielerHosten(5, 5)
+
+                elif Kords[0]>text_lokal_x and Kords[0]<text_lokal_x+400 and Kords[1]>text_lokal_y and Kords[1]<text_lokal_y+75:
+                    G.current_view = MehrspielerLokal(5, 5)
 
                 elif Kords[0]>text_beenden_x and Kords[0]<text_beenden_x+250 and Kords[1]>text_beenden_y and Kords[1]<text_beenden_y+75:
                     pygame.quit()
 
-        text_spielen = prettyfont.render("Spielen", True, colors.orange)
+        text_raumbeitreten = prettyfont.render("Mehrspieler - Raum beitreten", True, colors.orange)
+        text_raumhosten = prettyfont.render("Mehrspieler - Raum hosten", True, colors.orange)
+        text_lokal = prettyfont.render("Mehrspieler - lokal", True, colors.orange)
         text_beenden = prettyfont.render("Beenden", True, colors.orange)
 
-        if Kords[0]>text_spielen_x and Kords[0]<text_spielen_x+200 and Kords[1]>text_spielen_y and Kords[1]<text_spielen_y+75:
-            text_spielen = prettyfont.render("Spielen", True, colors.orange_light)
+        if Kords[0]>text_raumbeitreten_x and Kords[0]<text_raumbeitreten_x+650 and Kords[1]>text_raumbeitreten_y and Kords[1]<text_raumbeitreten_y+75:
+            text_raumbeitreten = prettyfont.render("Mehrspieler - Raum beitreten", True, colors.orange_light)
+            text_raumhosten = prettyfont.render("Mehrspieler - Raum hosten", True, colors.orange)
+            text_lokal = prettyfont.render("Mehrspieler - lokal", True, colors.orange)
+            text_beenden = prettyfont.render("Beenden", True, colors.orange)
+
+        elif Kords[0]>text_raumhosten_x and Kords[0]<text_raumhosten_x+600 and Kords[1]>text_raumhosten_y and Kords[1]<text_raumhosten_y+75:
+            text_raumbeitreten = prettyfont.render("Mehrspieler - Raum beitreten", True, colors.orange)
+            text_raumhosten = prettyfont.render("Mehrspieler - Raum hosten", True, colors.orange_light)
+            text_lokal = prettyfont.render("Mehrspieler - lokal", True, colors.orange)
+            text_beenden = prettyfont.render("Beenden", True, colors.orange)
+
+        elif Kords[0]>text_lokal_x and Kords[0]<text_lokal_x+400 and Kords[1]>text_lokal_y and Kords[1]<text_lokal_y+75:
+            text_raumbeitreten = prettyfont.render("Mehrspieler - Raum beitreten", True, colors.orange)
+            text_raumhosten = prettyfont.render("Mehrspieler - Raum hosten", True, colors.orange)
+            text_lokal = prettyfont.render("Mehrspieler - lokal", True, colors.orange_light)
             text_beenden = prettyfont.render("Beenden", True, colors.orange)
 
         elif Kords[0]>text_beenden_x and Kords[0]<text_beenden_x+250 and Kords[1]>text_beenden_y and Kords[1]<text_beenden_y+75:
-            text_spielen = prettyfont.render("Spielen", True, colors.orange)
+            text_raumbeitreten = prettyfont.render("Mehrspieler - Raum beitreten", True, colors.orange)
+            text_raumhosten = prettyfont.render("Mehrspieler - Raum hosten", True, colors.orange)
+            text_lokal = prettyfont.render("Mehrspieler - lokal", True, colors.orange)
             text_beenden = prettyfont.render("Beenden", True, colors.orange_light)
 
-        screen.blit(text_spielen, (text_spielen_x, text_spielen_y))
+        screen.blit(text_raumbeitreten, (text_raumbeitreten_x, text_raumbeitreten_y))
+        screen.blit(text_raumhosten, (text_raumhosten_x, text_raumhosten_y))
+        screen.blit(text_lokal, (text_lokal_x, text_lokal_y))
         screen.blit(text_beenden, (text_beenden_x, text_beenden_y))
 
         pygame.display.update()
@@ -78,7 +108,7 @@ class Spielende:
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if Kords[0]>text_spielen_x and Kords[0]<text_spielen_x+200 and Kords[1]>text_spielen_y and Kords[1]<text_spielen_y+75:
-                    G.current_view = ActualGame(5, 5)
+                    G.current_view = MehrspielerLokal(5, 5)
 
                 elif Kords[0]>text_beenden_x and Kords[0]<text_beenden_x+250 and Kords[1]>text_beenden_y and Kords[1]<text_beenden_y+75:
                     pygame.quit()
@@ -88,11 +118,11 @@ class Spielende:
         text_beenden = prettyfont.render("Beenden", True, colors.orange)
 
         if Kords[0]>text_spielen_x and Kords[0]<text_spielen_x+200 and Kords[1]>text_spielen_y and Kords[1]<text_spielen_y+75:
-            text_spielen = prettyfont.render("Spielen", True, colors.orange_light)
+            text_spielen = prettyfont.render("erneut spielen", True, colors.orange_light)
             text_beenden = prettyfont.render("Beenden", True, colors.orange)
 
         elif Kords[0]>text_beenden_x and Kords[0]<text_beenden_x+250 and Kords[1]>text_beenden_y and Kords[1]<text_beenden_y+75:
-            text_spielen = prettyfont.render("Spielen", True, colors.orange)
+            text_spielen = prettyfont.render("erneut spielen", True, colors.orange)
             text_beenden = prettyfont.render("Beenden", True, colors.orange_light)
 
         screen.blit(text_gewinneranzeige, (text_gewinneranzeige_x, text_gewinneranzeige_y))
@@ -101,7 +131,7 @@ class Spielende:
 
         pygame.display.update()
 
-class ActualGame():
+class MehrspielerLokal():
     def __init__(self, AnzahlSpalten, AnzahlZeilen):
 
         self.current_Spielbrett = mainclasses.Spielbrett(AnzahlSpalten, AnzahlZeilen, 20)
@@ -168,11 +198,158 @@ class ActualGame():
         self.current_Spielbrett.show(screen)
         pygame.display.update()
 
+class MehrspielerHosten:
+    def __init__(self, AnzahlSpalten, AnzahlZeilen):
+
+        self.current_Spielbrett = mainclasses.Spielbrett(AnzahlSpalten, AnzahlZeilen, 20)
+        self.current_Spieler1 = mainclasses.Spieler(1, colors.lime_green)
+        self.current_Spieler2 = mainclasses.Spieler(2, colors.dark_red)
+        self.am_Zug = self.current_Spieler1
+
+        self.serverport = 55555
+        self.server = networkutils.käsekästchenserver(self.serverport, AnzahlSpalten, AnzahlZeilen)
+        self.server.start()
+
+        self.client = networkutils.käsekästchenclient(self, ("localhost", self.serverport))
+
+
+        self.indizes_paar_angeklickter_punkte = []
+
+    def view(self):
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+
+                Kords = pygame.mouse.get_pos()
+                indices = self.current_Spielbrett.IndicesVonAngeklicktemPunktReturnieren(Kords[0], Kords[1])
+                if indices!=None:
+                    self.indizes_paar_angeklickter_punkte.append(indices)
+
+                if (len(self.indizes_paar_angeklickter_punkte)==2):
+                    if (horizontaldervertikalbenachbart(self.indizes_paar_angeklickter_punkte)):
+                        if not(self.current_Spielbrett.angeklicktePunkteExistierenSchonAlsVerbindung(self.indizes_paar_angeklickter_punkte)):
+                            kordsangeklicktezweipunkte = self.current_Spielbrett.KordsAngeklicktePunkteReturnieren(self.indizes_paar_angeklickter_punkte)
+                            gewonnenepunkte = self.current_Spielbrett.VerbindungHinzufuegen(self.indizes_paar_angeklickter_punkte, kordsangeklicktezweipunkte, self.am_Zug.ID, self.am_Zug.verbindungsfarbe)
+
+                            if gewonnenepunkte>0:
+                                self.am_Zug.Punkte += gewonnenepunkte
+
+                            else:
+
+                                if self.am_Zug.ID == self.current_Spieler1.ID:
+                                    self.am_Zug = self.current_Spieler2
+                                elif self.am_Zug.ID == self.current_Spieler2.ID:
+                                    self.am_Zug = self.current_Spieler1
+
+                    self.indizes_paar_angeklickter_punkte = []
+
+                if self.current_Spielbrett.verbindungen.Laenge()==(self.current_Spielbrett.AnzahlKästchenHo-1)*(self.current_Spielbrett.AnzahlKästchenVer) + (self.current_Spielbrett.AnzahlKästchenVer-1)*(self.current_Spielbrett.AnzahlKästchenHo):
+                    if (self.current_Spieler1.Punkte>self.current_Spieler2.Punkte):
+                        G.current_view = Spielende("Spieler 1", self.current_Spieler1.verbindungsfarbe)
+                    elif (self.current_Spieler2.Punkte>self.current_Spieler1.Punkte):
+                        G.current_view = Spielende("Spieler 2", self.current_Spieler2.verbindungsfarbe)
+                    else:
+                        G.current_view = Spielende("Unentschiden", colors.white)
+
+
+        screen.fill(colors.orange)
+
+        text_amzug = None
+        if (self.am_Zug.ID==1):
+            text_amzug = prettyfontsmaller.render("Spieler 1 ist dran!", True, self.am_Zug.verbindungsfarbe)
+        elif (self.am_Zug.ID==2):
+            text_amzug = prettyfontsmaller.render("Spieler 2 ist dran!", True, self.am_Zug.verbindungsfarbe)
+        screen.blit(text_amzug, (600, 200))
+
+        text_Spieler1Punkte = prettyfont.render(str(self.current_Spieler1.Punkte), True, self.current_Spieler1.verbindungsfarbe)
+        text_Spieler2Punkte = prettyfont.render(str(self.current_Spieler2.Punkte), True, self.current_Spieler2.verbindungsfarbe)
+        screen.blit(text_Spieler1Punkte, (640, 75))
+        screen.blit(text_Spieler2Punkte, (740, 75))
+
+        self.current_Spielbrett.show(screen)
+        pygame.display.update()
+
+class MehrspielerRaumbeireten():
+    def __init__(self, AnzahlSpalten, AnzahlZeilen, HostnameServer):
+
+        self.current_Spielbrett = mainclasses.Spielbrett(AnzahlSpalten, AnzahlZeilen, 20)
+        self.current_Spieler1 = mainclasses.Spieler(1, colors.lime_green)
+        self.current_Spieler2 = mainclasses.Spieler(2, colors.dark_red)
+        self.am_Zug = self.current_Spieler1
+        self.HostnameServer = HostnameServer
+
+        self.indizes_paar_angeklickter_punkte = []
+
+    def view(self):
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+
+                Kords = pygame.mouse.get_pos()
+                indices = self.current_Spielbrett.IndicesVonAngeklicktemPunktReturnieren(Kords[0], Kords[1])
+                if indices!=None:
+                    self.indizes_paar_angeklickter_punkte.append(indices)
+
+                if (len(self.indizes_paar_angeklickter_punkte)==2):
+                    if (horizontaldervertikalbenachbart(self.indizes_paar_angeklickter_punkte)):
+                        if not(self.current_Spielbrett.angeklicktePunkteExistierenSchonAlsVerbindung(self.indizes_paar_angeklickter_punkte)):
+                            kordsangeklicktezweipunkte = self.current_Spielbrett.KordsAngeklicktePunkteReturnieren(self.indizes_paar_angeklickter_punkte)
+                            gewonnenepunkte = self.current_Spielbrett.VerbindungHinzufuegen(self.indizes_paar_angeklickter_punkte, kordsangeklicktezweipunkte, self.am_Zug.ID, self.am_Zug.verbindungsfarbe)
+
+                            if gewonnenepunkte>0:
+                                self.am_Zug.Punkte += gewonnenepunkte
+
+                            else:
+
+                                if self.am_Zug.ID == self.current_Spieler1.ID:
+                                    self.am_Zug = self.current_Spieler2
+                                elif self.am_Zug.ID == self.current_Spieler2.ID:
+                                    self.am_Zug = self.current_Spieler1
+
+                    self.indizes_paar_angeklickter_punkte = []
+
+                if self.current_Spielbrett.verbindungen.Laenge()==(self.current_Spielbrett.AnzahlKästchenHo-1)*(self.current_Spielbrett.AnzahlKästchenVer) + (self.current_Spielbrett.AnzahlKästchenVer-1)*(self.current_Spielbrett.AnzahlKästchenHo):
+                    if (self.current_Spieler1.Punkte>self.current_Spieler2.Punkte):
+                        G.current_view = Spielende("Spieler 1", self.current_Spieler1.verbindungsfarbe)
+                    elif (self.current_Spieler2.Punkte>self.current_Spieler1.Punkte):
+                        G.current_view = Spielende("Spieler 2", self.current_Spieler2.verbindungsfarbe)
+                    else:
+                        G.current_view = Spielende("Unentschiden", colors.white)
+
+
+        screen.fill(colors.orange)
+
+        text_amzug = None
+        if (self.am_Zug.ID==1):
+            text_amzug = prettyfontsmaller.render("Spieler 1 ist dran!", True, self.am_Zug.verbindungsfarbe)
+        elif (self.am_Zug.ID==2):
+            text_amzug = prettyfontsmaller.render("Spieler 2 ist dran!", True, self.am_Zug.verbindungsfarbe)
+        screen.blit(text_amzug, (600, 200))
+
+        text_Spieler1Punkte = prettyfont.render(str(self.current_Spieler1.Punkte), True, self.current_Spieler1.verbindungsfarbe)
+        text_Spieler2Punkte = prettyfont.render(str(self.current_Spieler2.Punkte), True, self.current_Spieler2.verbindungsfarbe)
+        screen.blit(text_Spieler1Punkte, (640, 75))
+        screen.blit(text_Spieler2Punkte, (740, 75))
+
+        self.current_Spielbrett.show(screen)
+        pygame.display.update()
 
 CLOCK = pygame.time.Clock()
 
 G = Game()
 G.current_view = Menu()
 while True:
-    G.current_view.view()
-    CLOCK.tick(30)
+    try:
+        G.current_view.view()
+        CLOCK.tick(30)
+
+    except KeyboardInterrupt:
+        if isinstance(G.current_view, MehrspielerHosten):
+            G.current_view.server.stop()
+        pygame.quit()
---
+++ Käsekästchen pygame/networkutils.py
@@ -0,0 +1,134 @@
+import socket
+from threading import Thread, Event
+
+import mainclasses
+import colors
+import datastructures
+from helper import *
+
+class käsekästchenserver:
+    def __init__(self, port, AnzahlSpalten, AnzahlZeilen):
+        self.current_Spielbrett = mainclasses.Spielbrett(AnzahlSpalten, AnzahlZeilen, 20)
+        self.current_Spieler1 = mainclasses.Spieler(1, colors.lime_green)
+        self.current_Spieler2 = mainclasses.Spieler(2, colors.dark_red)
+        self.current_Spieler1_hostname = None
+        self.current_Spieler2_hostname = None
+        self.am_Zug = self.current_Spieler1
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.address = ("localhost", port)
+        self.mainthread = Thread(target=self.handleRequests, args=())
+        self.running = None
+
+    def start(self):
+        self.mainthread.start()
+        self.running = True
+
+    def stop(self):
+        self.running = False
+        self.mainthread.join()
+
+    def handleRequests(self):
+        self.sock.bind(self.address)
+        self.sock.listen()
+        self.sock.settimeout(0.2)
+        print("Lokalen Server erfolgreich gestartet auf Port ", self.address[1])
+
+        while self.running:
+
+            try:
+                connection, client_address = self.sock.accept()
+                print(client_address)
+
+                if self.current_Spieler1_hostname==None or self.current_Spieler2_hostname==None: #wenn hostnamen noch nicht bekannt sind
+
+                    if client_address[0]=="127.0.0.1":
+                        self.current_Spieler1_hostname = client_address[0]
+                        print("sending answer to client")
+                        self.sendToConnection(connection, "1")
+                    else:
+                        self.current_Spieler2_hostname = client_address[0]
+                        self.sendToConnection(connection, "1")
+
+                else:   #wenn hostnamen von beiden Spieleclients jedoch bekannt sind
+
+                    data = connection.recv()
+                    request_parts = data.decode("utf-8").split(" ")
+                    operation = request_parts[0]
+                    args = request_parts[1].split(",")
+
+                    if operation=="add_connection":
+                        indices_ersterPunkt = (args[0], args[1])
+                        indices_zweiterPunkt = (args[2], args[3])
+                        indizes_paar_angeklickter_punkte = [indices_ersterPunkt, indices_zweiterPunkt]
+
+                        if client_address[0]==self.current_Spieler1_hostname: #wenn anfrage von spieler 1 (Hostgerät) kommt
+                            if self.am_Zug.ID==1: #wenn spieler 1 auch dran ist dann füge verbindung hinzu
+                                VerbindungSpielbrettHinzufügen(self, indizes_paar_angeklickter_punkte, self.am_Zug)
+                                self.sendToConnection(connection, "2")
+
+                            else:                 #wenn spieler 1 jedoch nicht dran sein sollte, dann füge keine Verbindung hinzu
+                                self.sendToConnection(connection, "1")
+
+
+                        elif client_address[0]==self.current_Spieler2_hostname: #wenn anfrage von spieler 2 (externes Gerät) kommt
+                            if self.am_Zug.ID==2: #wenn spieler 2 auch dran ist dann füge verbindung hinzu
+                                VerbindungSpielbrettHinzufügen(self, indizes_paar_angeklickter_punkte, self.am_Zug)
+                                self.sendToConnection(connection, "2")
+
+                            else:                 #wenn spieler 2 jedoch nicht dran sein sollte, dann füge keine Verbindung hinzu
+                                self.sendToConnection(connection, "1")
+
+                        else:   #wenn anfage von keinem der beiden spieleclients kommt
+                            self.sendToConnection(connection, "0")
+
+                        connection.close()
+
+                    elif operation=="update_gamestate":
+                        pass
+
+
+            except socket.timeout:
+                pass
+
+            except:
+                break
+
+        self.sock.close()
+
+        def sendToConnection(self, conn, string):
+            conn.sendall(string.encode())
+
+        def VerbindungSpielbrettHinzufügen(self, indizes_paar_angeklickter_punkte, am_Zug):
+            if (horizontaldervertikalbenachbart(indizes_paar_angeklickter_punkte)):
+                if not(self.current_Spielbrett.angeklicktePunkteExistierenSchonAlsVerbindung(indizes_paar_angeklickter_punkte)):
+                    kordsangeklicktezweipunkte = self.current_Spielbrett.KordsAngeklicktePunkteReturnieren(indizes_paar_angeklickter_punkte)
+                    gewonnenepunkte = self.current_Spielbrett.VerbindungHinzufuegen(self.indizes_paar_angeklickter_punkte, kordsangeklicktezweipunkte, am_Zug.ID, am_Zug.verbindungsfarbe)
+
+class käsekästchenclient:
+
+    def __init__(self, view, server_address):
+        self.view = view
+        self.server_address = server_address
+        self.mainthread = None
+        self.running = None
+
+        self.mainthread = Thread(target=self.pingeserveran, args=())
+        self.mainthread.start()
+        self.running = True
+
+    def pingeserveran(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect(self.server_address)
+            statuscode = None
+
+            status = sock.recv(1024)
+            print(status)
+        #while statuscode==None:
+        #    received = sock.recv(16).decode("utf-8")
+        #    print(received)
+
+        #if received!="":
+        #        statuscode = received
+
+        sock.close()
