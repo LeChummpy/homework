e8e948cdc7af117fdf49209ed244b692406fe9d4
LeChummpy <benjamin.jank@gmx.de>
2022-08-18 20:39:59 +0200

    asdds

---
+++ circuits/von-neumann/assembler/15bit/assembler.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main (int argc, char*argv[]) {
+  std::ifstream myfile;
+  //myfile.open();
+
+  printf ("adresse: %s", argv[0])
+
+  return 0
+}
